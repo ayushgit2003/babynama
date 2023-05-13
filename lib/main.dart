fa8c521d8bcc55fce85d65c0ab42453c7c040705
ayushgit2003 <ayush123tripathi.mt@gmail.com>
2023-05-14 04:41:59 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,79 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Demo Home Page'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+
+  // This widget is the home page of your application. It is stateful, meaning
+  // that it has a State object (defined below) that contains fields that affect
+  // how it looks.
+
+  // This class is the configuration for the state. It holds the values (in this
+  // case the title) provided by the parent (in this case the App widget) and
+  // used by the build method of the State. Fields in a Widget subclass are
+  // always marked "final".
+
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: EdgeInsets.only(right: 8,top : 4),
+      child: Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          backgroundColor: Colors.black,
+          title: SizedBox(
+              height: 80, width: 135, child:Image.asset('assets/logo-new.jpeg',) ),
+          actions: [
+            Center(
+              child: Container(
+                height: 47,
+                width: 47,
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  border: Border.all(color: Colors.white54, width :3,)
+                ),
+                child: IconButton(
+                  icon: const Icon(
+                    Icons.density_medium,
+                    color: Colors.white54,
+                    size: 25,
+                  ),
+                  onPressed: () {
+                    // do something
+                  },
+                ),
+              ),
+            )
+          ],
+        ),
+        body: ,
+      ),
+    );
+  }
+}
